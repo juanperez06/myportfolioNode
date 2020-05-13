5f5c052226eaa1e1894b6ab5d44eefc203983905
juanperez06 <juan.perez06@outlook.com>
2020-05-13 19:43:01 -0400

    modified files structure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const webRoutes = require('./routes/web.js')
+const webRoutes = require('./server/routes/web.js')
 const pug = require('pug')
 const path = require('path')
 
@@ -11,13 +11,11 @@ require('dotenv').config()
 
 app.use(express.static('client/public'))
 app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, '../client/src/views'))
+app.set('views', path.join(__dirname, './client/src/views'))
 
 
 app.use(webRoutes)
 
 
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on http://localhost:3000/ to stop server press control + c ' + process.env.DB_USERNAME)
-})
+server.listen(process.env.PORT || 3000)
