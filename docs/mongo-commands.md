993f039170257d7429c35bd604ad188e2c9da449
juanperez06 <juan.perez06@outlook.com>
2020-05-16 11:21:43 -0400

    fixed some bugs

---
+++ docs/mongo-commands.md
@@ -0,0 +1,17 @@
+Commands
+
+mongod - start mongo
+
+mongo - go to commandline of mongodb
+
+help - show all commands
+
+use - use a specific database or create a new one
+
+show dbs - show all databases
+
+insert - insert a document into a collection or create a new one
+
+find - find a specific document or show all documents
+
+remove - remove a document
--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const PostModel = require('../models/Post').model
 
 const blogController = {}
 
@@ -8,6 +9,19 @@ blogController.index = (req, res) => {
 
 blogController.show = (req, res) => {
     res.render('blog/show')
+}
+
+blogController.create = (req, res) => {
+    res.render('blog/create')
+}
+
+blogController.store = (req, res) => {
+    PostModel.create({
+        title: 'My First Post',
+        description: 'Hey this is a cool post about nothing'   
+    }, function (err, data){
+        console.log(data)
+    })
 }
 
 module.exports = blogController
